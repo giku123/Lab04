6e1dbe2ae0333a8e8efc08ab8fbcb3dae8fb2ec9
Ispas Gheorghe <72625185+giku123@users.noreply.github.com>
2024-11-14 17:11:30 +0200

    direction.cpp

---
+++ direction.cpp
@@ -0,0 +1,11 @@
+#include "direction.hpp"
+
+std::ostream& operator<<(std::ostream& out, const Direction& direction) {
+    switch (direction) {
+        case Direction::Top: out << "Top"; break;
+        case Direction::Left: out << "Left"; break;
+        case Direction::Right: out << "Right"; break;
+        case Direction::Bottom: out << "Bottom"; break;
+    }
+    return out;
+}
