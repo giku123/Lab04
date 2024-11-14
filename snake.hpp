c3c97d2a86fa1a8cccabe11c18f2b7638a9f5027
Ispas Gheorghe <72625185+giku123@users.noreply.github.com>
2024-11-14 17:12:12 +0200

    snake.hpp

---
+++ snake.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <deque>
+#include "point.hpp"
+#include "direction.hpp"
+#include "apple.hpp"
+
+class Snake {
+private:
+    std::deque<Point> body;
+    Direction currentDirection;
+
+public:
+    Snake();
+    void Move();
+    void Grow();
+    void SetDirection(Direction dir);
+    const std::deque<Point>& GetBody() const;
+    bool IsColliding() const;
+    bool HasEaten(const Apple& apple);
+};
